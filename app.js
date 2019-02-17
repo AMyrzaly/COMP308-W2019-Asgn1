a71d2517af94fa546e7c6e72b45f666d2f5c4ea1
Abubakir Myrzaly <best.abuka@gmail.com>
2019-02-17 12:16:54 -0500

    Made changes to index.ejs

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
-
+app.use(express.static(path.join(__dirname, '../models')));
 app.use('/', indexRouter);
 app.use('/contact-list', contactRouter);
 
